055616bd56d05af8e51466e6add6098c84cfd5b7
taha2oo2 <106246522+taha2oo2@users.noreply.github.com>
2022-11-29 13:54:55 +0500

    Update text.js

--- text.js
+++ text.js
@@ -1,16 +1,14 @@
- var num= Math.round(Math.random() *10); 
-//  var n =num === b; 
- for (var i=0;i<=10;i++){
- var b = +prompt("Enter a number");
-if(num === b){
-    alert("Correct Guess");
-    break;
-} else{
-    alert(" Try Again");
-    continue;
-}     } 
-
-
+// var num= Math.round(Math.random() *10); 
+// //  var n =num === b; 
+//  for (var i=0;i<=10;i++){
+//  var b = +prompt("Enter a number");
+// if(num === b){
+//     alert("Correct Guess");
+//     break;
+// } else{
+//     alert(" Try Again");
+//     continue;
+// }     } 
 // else if( num - 1 === b){
 //     alert("Close Enough!! but the num was:" +num);
 
@@ -25,4 +23,61 @@ if(num === b){
 //     console.log("comp:" +num)
 //     console.log("user:" +b)
 //  }while(!(num===b));
+//*********************************************************************************************** */
+//Functions
+// function Add(x,y){
+//     return x+y;
+// }
+
+// alert(Add(3,4));
+
+// function Round(x){
+//     return Math.round(x);
+// }
+
+// alert(Round(4.9));
+//************************************************************************************************ */
+// Local/Global Var
+// var a=23;
+// function demo(){
+//     console.log(a);
+//     alert(a);
+//     a=10;
+    
+// } 
+// demo();
+//************************************************************************************************** */
+// Switch case
+// var a;
+// a= prompt("Enter a Num:")
+// switch(a){
+// case "1":
+//     alert("You Entered 1");
+//     break;
+//     case "3":
+//         alert("You Entered 3");
+//         break;
+//         case "4":
+//             alert("You Entered 4");
+//             break;
+//             case "5":
+//                 alert("You Entered 5");
+//                 break;
+//                 case "2":
+//                     alert("You Entered 2");
+//                     break;
+//                     default:
+//                         alert("Enter in range 1-5");
+//************************************************************************************************** */
+//While & Do while Loop
+// var i;
+// i= +prompt("Enter a num");
+// while(i>0){
+//     alert("Correct");
+//     console.log(i);
+//     i--;
+// }
+//************************************************************************************************** */
+
+    
 
